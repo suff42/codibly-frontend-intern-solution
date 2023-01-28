2265131d4dafb317c572eeaebef84e13a6427023
Radek Piotrowski <radek.rpiotrowski@gmail.com>
2023-01-28 23:03:06 +0100

    make Pagination a controlled component

--- src/components/PageNumber/PageNumber.tsx
+++ src/components/PageNumber/PageNumber.tsx
@@ -4,15 +4,21 @@ import { ProductsContext } from "../../context/ProductsContext";
 import { ProductContextType } from "../../context/ProductContext.types";
 
 const PageNumber = () => {
-  const { totalPages, setCurrentPage } = useContext(
+  const { totalPages, currentPage, setCurrentPage } = useContext(
     ProductsContext
   ) as ProductContextType;
 
-  const hanglePageChange = (e: React.ChangeEvent<unknown>, value: number) => {
+  const handlePageChange = (e: React.ChangeEvent<unknown>, value: number) => {
     setCurrentPage(value);
   };
 
-  return <Pagination count={totalPages} onChange={hanglePageChange} />;
+  return (
+    <Pagination
+      count={totalPages}
+      page={currentPage}
+      onChange={handlePageChange}
+    />
+  );
 };
 
 export default PageNumber;
